a7d3eceb587f43c126fc4a78912767cd2c9842a1
Target04 <obbi.vinci@gmail.com>
2024-11-27 17:44:50 +0100

    First commit

---
+++ src/main/java/com/mycompany/progetto_is/Progetto_IS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.progetto_is;
+
+/**
+ *
+ * @author obbiv
+ */
+public class Progetto_IS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
